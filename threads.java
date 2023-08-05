ca6fe9fcc70ea8bf90a187a0fb4cb9e213abd30c
Karen Knup <106675809+KarenKnup@users.noreply.github.com>
2023-08-05 13:28:42 -0300

    Update threads.java

--- threads.java
+++ threads.java
@@ -115,3 +115,22 @@ public class App{
       ob.start();
    }
 }
+
+How many lines of output will this code produce? //2
+
+class B implements Runnable {
+   public void run() {
+     System.out.println("B");	
+   }
+}
+class A extends Thread {
+   public void run() {
+    System.out.println("A");
+    Thread t = new Thread(new B());
+    t.start();
+   }
+public static void main(String[ ] args) {
+   A object = new A();
+   object.start();
+   }
+}
